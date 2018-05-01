975904b0e642b8e8b980eae73c29519134d33ca4
Razvan Ludosanu <r.ludosanu@gmail.com>
2018-05-01 09:54:45 -0700

    API Routes added, forgot page added

--- server/index.js
+++ server/index.js
@@ -8,18 +8,51 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static('views'));
 
-app.get('/', function (req, res) {
+/*
+** Basic Routes
+*/
+app.get('/', function(req, res) {
 	res.render('login.html');
 });
 
-app.get('/register', function (req, res) {
+app.get('/register', function(req, res) {
 	res.render('register.html');
 });
 
-app.get('/activate', function (req, res) {
+app.get('/activate', function(req, res) {
 	res.render('activate.html');
 });
 
-app.listen(3000, function () {
+app.get('/forgot', function(req, res) {
+	res.render('forgot.html');
+});
+
+/*
+** API Routes
+*/
+app.post('/api/user/login', function(req, res) {
+	//
+});
+
+app.post('/api/user/register', function(req, res) {
+	//
+});
+
+app.put('/api/user/activate', function(req, res) {
+	//
+});
+
+app.post('/api/user/forgot/reset', function(req, res) {
+	//
+});
+
+app.post('/api/user/forgot/confirm', function(req, res) {
+	//
+});
+
+/*
+** Start server
+*/
+app.listen(3000, function() {
 	console.log('Example app listening on port 3000!')
 });
